cc707e30a2c17cb29fe8a62498e12229a0246721
janisgee <janisgeegee@gmail.com>
2025-03-17 21:54:33 +0800

    Added cache struct and test

--- main.go
+++ main.go
@@ -1,45 +1,21 @@
 package main
 
 import (
-	"bufio"
-	"fmt"
-	"os"
 	"strings"
+	"time"
+
+	"github.com/janisgee/pokedexcli-app/internal/pokeapi"
 )
 
 func main() {
-	fmt.Print("Pokedex > ")
-	reader := bufio.NewScanner(os.Stdin)
 
-	// Create config
-	cfg := &config{
-		NextURL: "https://pokeapi.co/api/v2/location-area",
+	// Create Config - httpClient and cache
+	pokeClient := pokeapi.NewClient(5*time.Second, 5*time.Minute)
+	cfg := &Config{
+		PokeApiClient: pokeClient,
 	}
+	startPokedex(cfg)
 
-	for reader.Scan() {
-		input := reader.Text()
-		trimInputList := cleanInput(input)
-
-		if len(trimInputList) == 0 {
-			continue
-		}
-
-		commandName := trimInputList[0]
-
-		commands := getCommands()
-
-		for i := range commands {
-			if commandName == commands[i].name {
-				err := commands[i].callback(cfg)
-				if err != nil {
-
-					fmt.Println(err)
-					return
-				}
-			}
-		}
-		fmt.Print("Pokedex > ")
-	}
 }
 
 func cleanInput(text string) []string {
